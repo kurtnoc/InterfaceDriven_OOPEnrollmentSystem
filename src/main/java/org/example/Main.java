fe9a9f485e453671cb4b5f3872b429de894cf659
Kurt Villapando <villapando.kurt27@gmail.com>
2026-05-09 00:50:31 +0800

    add switch case options for menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.model.*;
+import org.example.service.*;
+import org.example.service.impl.*;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    static IStudentService studentService = new StudentServiceImpl();
+    static IInstructorService instructorService = new InstructorServiceImpl();
+    static ICourseService courseService = new CourseServiceImpl();
+    static IEnrollmentService enrollmentService = new EnrollmentServiceImpl(studentService);
+    static ITuitionService tuitionService = new TuitionServiceImpl(studentService, enrollmentService);
+
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            System.out.println("\n======================================");
+            System.out.println("===  INTERFACE-DRIVEN ENROLLMENT   ===");
+            System.out.println("======================================");
+            System.out.println("[0] Exit");
+            System.out.println("[1] Student Management");
+            System.out.println("[2] Instructor Management");
+            System.out.println("[3] Course Management");
+            System.out.println("[4] Department & Section Management");
+            System.out.println("[5] Enrollment");
+            System.out.println("[6] Tuition Fee Management");
+            System.out.print("Choice: ");
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 -> studentMenu();
+                case 2 -> instructorMenu();
+                case 3 -> courseMenu();
+                case 4 -> departmentMenu();
+                case 5 -> enrollmentMenu();
+                case 6 -> tuitionMenu();
+                case 0 -> { System.out.println("Goodbye!"); running = false; }
+                default -> System.out.println("Invalid choice.");
+            }
         }
     }
-}
