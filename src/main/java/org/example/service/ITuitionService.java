a80f466f4c229668c056fa44912d87ef16ea7aa1
Kurt Villapando <villapando.kurt27@gmail.com>
2026-05-10 00:06:52 +0800

    Add Scholarship Discount Feature method in Interface

--- src/main/java/org/example/service/ITuitionService.java
+++ src/main/java/org/example/service/ITuitionService.java
@@ -4,6 +4,8 @@ import org.example.model.TuitionFeePayment;
 
 public interface ITuitionService {
     double calculateFee(int studentId, double ratePerUnit);
+    //Scholarship Feature
+    void grantScholarship(int studentId);
     void makePayment(int studentId, double amount);
     double getRemainingBalance(int studentId);
     TuitionFeePayment getPaymentRecord(int studentId);
