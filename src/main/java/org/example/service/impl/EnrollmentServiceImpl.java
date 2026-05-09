aee1ff3bffdc73591140e7d8b349ac07bb84ac15
Kurt Villapando <villapando.kurt27@gmail.com>
2026-05-09 23:44:40 +0800

    Add Prerequisite Check in Enrollment ServiceImpl

--- src/main/java/org/example/service/impl/EnrollmentServiceImpl.java
+++ src/main/java/org/example/service/impl/EnrollmentServiceImpl.java
@@ -50,6 +50,17 @@ public class EnrollmentServiceImpl implements IEnrollmentService {
         boolean alreadyEnrolled = section.getEnrolledStudents().stream()
                 .anyMatch(s -> s.getId() == studentId);
         if (alreadyEnrolled) { System.out.println("Student already enrolled in this section."); return; }
+//Check Prerequisite Feature
+        for (Course course : section.getCourses()) {
+            Integer prereqId = course.getPrerequisiteCourseId();
+            if (prereqId != null && !studentService.hasPassedCourse(student.getId(), prereqId)) {
+                System.out.printf(
+                        "ENROLLMENT REJECTED: %s has not passed the prerequisite (Course ID: %d) for '%s'.%n",
+                        student.getName(), prereqId, course.getCourseName()
+                );
+                return;
+            }
+        }
 
         section.getEnrolledStudents().add(student);
         System.out.printf("SUCCESS: %s enrolled in section '%s' (%d/%d).%n",
