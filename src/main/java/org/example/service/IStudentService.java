46f4f4be511a0150235d282eea8e73d4a922532c
Kurt Villapando <villapando.kurt27@gmail.com>
2026-05-09 23:17:59 +0800

    Declare methods of passed course in IStudentService Interface

--- src/main/java/org/example/service/IStudentService.java
+++ src/main/java/org/example/service/IStudentService.java
@@ -6,6 +6,9 @@ public interface IStudentService {
     void addStudent(Student student);
     void updateStudent(Student student);
     void removeStudent(int studentId);
+//Passed course feature
+    void markCoursePassed(int studentId, int courseId);
+    boolean hasPassedCourse(int studentId, int courseId);
     List<Student> getAllStudents();
     Student findById(int studentId);
 }
